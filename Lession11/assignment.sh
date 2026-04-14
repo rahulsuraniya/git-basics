bac1e0c3baef0ad1f3a829643f18e3651f64c0ad
Rahul <rahulsuraniyagautam@gmail.com>
2026-04-14 20:48:37 +0530

    Lession11 updated deatails

---
+++ Lession11/assignment.sh
@@ -0,0 +1,19 @@
+: << 'INFO'
+
+Assignment - Chapter 11(Important)
+
+Tasks:
+1. Create compose-practice directory
+2. Create docker-compose.yml
+3. Run nginx using compose on port 8085
+4. Start in detached mode
+5. Verify in browser
+6. Stop & remove containers
+
+
+Commands reminder:
+1. docker-compose up -d
+2. docker ps
+3. docker-compose down
+
+INFO
